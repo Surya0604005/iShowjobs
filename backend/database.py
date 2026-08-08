911c144d4356e834cf42ce467b36da36c286496e
Surya Teja Katta <kattasuryateja19@gmail.com>
2026-08-08 15:41:59 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -10,12 +10,10 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT", "19068")
 DB_NAME = os.getenv("DB_NAME", "defaultdb")
 
-
 DATABASE_URL = (
     f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}"
     f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
-
 
 engine = create_engine(
     DATABASE_URL,
@@ -24,20 +22,17 @@ engine = create_engine(
     }
 )
 
-
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
 
-
 Base = declarative_base()
 
 
 def get_db():
     db = SessionLocal()
-
     try:
         yield db
     finally:
