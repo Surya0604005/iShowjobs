248d4d0514a58ef0004ec4a410100e77fdd54c04
Surya Teja Katta <kattasuryateja19@gmail.com>
2026-08-13 18:44:16 +0530

    api

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ishowjobs-api.onrender.com",
+  baseURL: "https://ishowjobs-backend.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
